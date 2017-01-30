d8f3462d3130bedd855fae948a0c50773c6a9bf7
Pankaj Singh Mathur <pankajmathur@Pankajs-MacBook-Pro.local>
2017-01-29 22:00:43 -0500

    Second commet
    Added backup file, updated original one to add numpy genfromtxt and
    array function because without them list type is not useful calculating
    partial derivates on matrices

--- simple_linear_regression_python_only.py
+++ simple_linear_regression_python_only.py
@@ -1,18 +1,29 @@
-#simple linear regression using python, no fancy liabraries and packages needed
+#simple linear regression using python, no fancy sklearn, scipy, pandas ackages needed
+# use only 2 functions from numpy
+from numpy import genfromtxt, array
+
+# we are goning to use Linear equation of type y = mx + b
+# where m is slope, and b is y-intercept, a constant
+# x is independent varaible and y is depdendent variable
+
+# The Step Gradient/ Slope Descent function, calcualates gradient of b and m
+def step_gradient(b_current, m_current, points, learningRate):
+	b_gradient = 0
+	m_gradient = 0
+	N = float(len(points))
+	for i in range(len(points)):
+		x = points[i,0]
+		y = points[i,1]
+		b_gradient += - (2/N) * (y - ((m_current * x) + b_current))
+		m_gradient += - (2/N) * x * (y - ((m_current * x) + b_current))
+
+	new_b = b_current - (learningRate * b_gradient)
+	new_m = m_current - (learningRate * m_gradient)
+
+	return [new_b, new_b]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# The Error function
 def compute_error_for_line_given_points(b, m, points):
 	
 	totalError = 0
@@ -26,12 +37,18 @@ def compute_error_for_line_given_points(b, m, points):
 
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
-	pass
+	
+	b = starting_b
+	m = starting_m
+	for i in range(num_iterations):
+		b, m = step_gradient(b,m, array(points), learning_rate)
+		return [b, m]
 
 
 def pankax():
-	
-	points = 'data here'
+
+	#data from data.csv cotaining number of study hours and GPA
+	points = genfromtxt("data.csv", delimiter=",")
 	
 	# choose learning rate
 	learning_rate = 0.0001
